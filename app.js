dedde8f6712318213c8221f55725364f9021f36e
odiroot <odi.root@gmail.com>
2013-09-29 20:43:37 +0200

    Cleaning the project a bit.

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+
+var routes = require('./routes');
 var mouse = require("./mouse");
 
 var app = express();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -20,18 +16,20 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser('kedavra'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
+
 app.get('/', routes.index);
-app.get('/users', user.list);
+
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
