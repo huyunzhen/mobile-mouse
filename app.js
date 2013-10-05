59df59907c50dab9eb1c2f1a675d24c13bfd5fbe
odiroot <odi.root@gmail.com>
2013-10-05 22:40:34 +0200

    Don't spam with request logging in production.

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+if ('development' == app.get('env')) {
+    app.use(express.logger('dev'));
+}
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('kedavra'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // development only
 if ('development' == app.get('env')) {
