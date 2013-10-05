b4cd7dfddba0cb621516344d40bb5aeb1feaf13d
odiroot <odi.root@gmail.com>
2013-10-05 23:11:56 +0200

    Acceleration is working properly.
    Also limiting movement to screen size.

--- mouse.js
+++ mouse.js
@@ -1,6 +1,9 @@
 var x11 = require("x11");
 
+var ACCEL = 1.75;
+
 var display = null;
+var screenSize = null;
 
 
 function init() {
@@ -11,6 +14,12 @@ function init() {
 
         display = d;
         console.log("Created X11 client.");
+
+        // Fetch viewport size.
+        var root = display.screen[0].root;
+        display.client.GetGeometry(root, function(err, result) {
+            screenSize = [result.width, result.height];
+        });
     });
 }
 
@@ -39,16 +48,25 @@ function currentPosition(callback) {
 
 function changePosition(x, y) {
     var root = display.screen[0].root;
-    display.client.WarpPointer(0, root, 0, 0, 0, 0, x, y);
+    display.client.WarpPointer(0, root, 0, 0, 0, 0, parseInt(x), parseInt(y));
 }
 
 
 function move(diffX, diffY) {
     currentPosition(function(current) {
+        // Pseudo-acceleration.
+        if(Math.abs(diffX) > 3) { diffX *= ACCEL; }
+        if(Math.abs(diffY) > 3) { diffY *= ACCEL; }
+
+        // Update position based on current one.
         var newX = current[0] + diffX;
         var newY = current[1] + diffY;
 
-        changePosition(newX, newY);
+        changePosition(
+            // Don't go outside the screen.
+            Math.min(newX, screenSize[0]),
+            Math.min(newY, screenSize[1])
+        );
     });
 }
 
